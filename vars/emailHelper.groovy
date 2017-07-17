6c45089edc47c481bb20988c441f7f94ea94a9b8
Alvaro Reig Gonzalez <alvaro.reig@catastro.minhap.es>
2017-07-17 17:18:09 +0200

    changing mailFrom

--- vars/emailHelper.groovy
+++ vars/emailHelper.groovy
@@ -6,14 +6,12 @@ def call(body) {
     body()
 
     node {
-        // Clean workspace before doing anything
-        //deleteDir()
 
         try {
 
             def user="api:${config.apiKey}"
             def url="https://api.mailgun.net/v3/${config.domainName}/messages"
-            def from="'Excited User <mailgun@${config.domainName}>'"
+            def from="'Mailgun Helper Jenkins <jenkins@${config.domainName}>'"
 
             println "sending email to ${config.mailTo}"
             sh "curl -s --user $user $url -F from=$from -F to=${config.mailTo} -F subject='${config.mailSubject}' -F text='${config.mailText}'"
